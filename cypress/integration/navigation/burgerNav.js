ea482e7ea1f03c9e73fcde74caed85e4e67d1f1f
Juan Gonzales <anomalyarcadesticks@gmail.com>
2022-09-04 10:58:35 -0700

    fixture

--- cypress/integration/navigation/burgerNav.js
+++ cypress/integration/navigation/burgerNav.js
@@ -1,5 +1,5 @@
 // Notes: Cypress runs Async in the background
-import example from "/Drata_UI/cypress/fixtures/example.json";
+//import example from "/Drata_UI/cypress/fixtures/example.json";
 import ResourcesPage from "../../support/pageObjects/resources/ResourcesPage";
 // let windowErrorSpy;
 // Cypress.on("window:before:load", (win) => {
@@ -22,12 +22,12 @@ describe("Burger Navigation", function () {
   //   });
   // });
 
-  example.sideNav.forEach((pair) => {
-    it(`Verifying ${pair[0]} / ${pair[1]} : Plus pm screen element contains: ${pair[2]}`, () => {
-      cy.visit("");
-      cy.navValidation(pair[0], pair[1], pair[2]);
-    });
-  });
+  // example.sideNav.forEach((pair) => {
+  //   it(`Verifying ${pair[0]} / ${pair[1]} : Plus pm screen element contains: ${pair[2]}`, () => {
+  //     cy.visit("");
+  //     cy.navValidation(pair[0], pair[1], pair[2]);
+  //   });
+  // });
   //Example test that uses stored WebElement in page objects as validation
 
   it("Verify Resources / All Case Studies Resources Contains: Case Studies Button", () => {
